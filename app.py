26d23409c137a63e07369f0efd1fd4cc6a83b770
Shovra <shovradas@gmail.com>
2020-03-06 08:48:13 +0100

    dataset added

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
 import json
-from flask import Flask
+from flask import Flask, Response
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    data = {'a': 10}
-    return json.dumps(data)
+    return 'IT WORKS!'
+
+
+@app.route('/ds1')
+def get_ds1():
+    with open('fp_ds_1.json', 'r', encoding='utf-8') as fp:
+        data = json.load(fp)
+    return Response(json.dumps(data, ensure_ascii=False), mimetype='application/json')
+
+
+@app.route('/ds2')
+def get_ds2():
+    with open('fp_ds_2.json', 'r', encoding='utf-8') as fp:
+        data = json.load(fp)
+    return Response(json.dumps(data, ensure_ascii=False), mimetype='application/json')
 
 
 if __name__ == "__main__":
